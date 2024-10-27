edf6f5fb84a7aaf4462ae983892c3fb038808c43
Anahita Sabaghi <anahita.rastegar@gmail.com>
2024-10-27 21:19:15 +0330

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const double bottomContainerHeight = 80;
-const activeCardColor = Color(0xFF1D1E33);
+const activeCardColor = Color(0xFF1C2033);
 const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
