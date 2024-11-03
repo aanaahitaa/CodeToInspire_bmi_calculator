9702295d536ee49c5f624fecb0d359339690d8bf
Anahita Sabaghi <anahita.rastegar@gmail.com>
2024-11-03 17:21:36 +0330

    Added icons, custom widgets, Gesture Detector, and Dart enums for improved interactivity.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,48 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const double bottomContainerHeight = 80;
-const activeCardColor = Color(0xFF1C2033);
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
 
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
+enum Gender{male, female}
 
+
+class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
+
 class _InputPageState extends State<InputPage> {
+
+  Gender? selectedGender;
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
+        title: Text('BMI CALCULATOR'),
       ),
       body: Column(
         children: [
-          const Expanded(
+          Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(color: activeCardColor),
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    child: iconContent(icon: FontAwesomeIcons.mars, label: 'MALE',),
+                    color: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(color: activeCardColor),
+                  child: ReusableCard(
+                    onPress: (){
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    child: iconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE',),
+                    color: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
+                  ),
                 ),
               ],
             ),
           ),
-          const Expanded(
-            child: ReusableCard(color: activeCardColor),
+          Expanded(
+            child: ReusableCard(),
           ),
-          const Expanded(
+          Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(color: activeCardColor),
+                  child: ReusableCard(),
                 ),
                 Expanded(
-                  child: ReusableCard(color: activeCardColor),
+                  child: ReusableCard(),
                 ),
               ],
             ),
@@ -58,7 +88,7 @@ class _InputPageState extends State<InputPage> {
               height: bottomContainerHeight,
               child: const Center(
                 child: Text(
-                  'CALCULATE',
+                  'CALCULATE YOUR BMI',
                   style: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
@@ -68,22 +98,6 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color color;
-  const ReusableCard({super.key, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
-import 'package:flutter/material.dart';
 import 'input_page.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(const BMICalculator());
+void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
-  const BMICalculator({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0D22),
-        scaffoldBackgroundColor: const Color(0xFF0A0D22),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF0A0D22),
-        ),
+        primaryColor: Color(0xFF070701),
+        scaffoldBackgroundColor:Color(0xFF070701),
       ),
-      home: const InputPage(),
+      home: InputPage(),
     );
   }
 }
